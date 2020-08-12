49747eef9aeb6094dd2ba0fc91f9aed564034cd3
Eric Lekwa <lekwaeric@yahoo.com>
2020-08-12 18:34:10 +0100

    Wrote Theme Helpers

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:dawai/core/theme/theme_helper.dart';
+import 'package:flutter/material.dart';
+
+Color overlay = HexColor("#EFF4FB");
+Color primaryColor = HexColor("#1686C5");
+Color primaryColorLight = HexColor("#DFEFFF");
+Color darkGray = HexColor("#515C6F");
+Color lightGray = HexColor("#9B9B9B");
+const Color white = const Color(0xffffffff);
+
+const Color transparent = Colors.transparent;
+
+//rgba(239, 244, 251, 1)
---
+++ lib/core/theme/font_styles.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+TextStyle dateText = TextStyle(
+    color: darkGray,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 31.0);
+TextStyle timeText = TextStyle(
+    color: primaryColor,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 15.0);
+TextStyle dateTextBold = TextStyle(
+    color: darkGray,
+    fontWeight: FontWeight.w700,
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 31.0);
+TextStyle dayText = TextStyle(
+    color: lightGray,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 16.0);
+TextStyle categoryTitle = TextStyle(
+    color: darkGray,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 17.0);
+TextStyle drugName = TextStyle(
+    color: darkGray,
+    fontWeight: FontWeight.w700,
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 16.0);
+TextStyle drugGrams = TextStyle(
+    color: lightGray,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 14.0);
+TextStyle navSelected = TextStyle(
+    color: primaryColor,
+    fontWeight: FontWeight.w700,
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 12.0);
+TextStyle navText = TextStyle(
+    color: lightGray,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 12.0);
